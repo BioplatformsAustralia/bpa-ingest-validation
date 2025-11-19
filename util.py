3b7f3081ec858518596803fd06a3bc1dde75b4a6
Brigette Gonch <b.gonch@qcif.edu.au>
2025-11-19 16:02:03 +1000

    Initial Commit

---
+++ util.py
@@ -0,0 +1,12 @@
+
+import logging
+def make_logger(name, level=logging.INFO):
+    logger = logging.getLogger(name)
+    logger.propagate = False
+    logger.setLevel(level)
+    handler = logging.StreamHandler()
+    fmt = logging.Formatter("%(asctime)s [%(levelname)-7s] [%(name)s]  %(message)s")
+    handler.setFormatter(fmt)
+    logger.addHandler(handler)
+    return logger
+
